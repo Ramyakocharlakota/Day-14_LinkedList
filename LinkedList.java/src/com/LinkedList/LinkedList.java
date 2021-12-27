2df6e5be64d5eb00fa80fd058722423b99d01479
Ramya kocharlakota <Ramyakocharlakota@gmail.com>
2021-12-27 09:18:22 +0530

    ADDED

---
+++ LinkedList.java/src/com/LinkedList/LinkedList.java
@@ -0,0 +1,25 @@
+package com.LinkedList;
+
+
+public class LinkedList {
+
+	class Main {
+	  public static void main(String[] args){
+
+	    // create linkedlist
+	    LinkedList numbers = new LinkedList();
+
+	    // Add elements to LinkedList
+	    numbers.add("56");
+	    numbers.add("30");
+	    numbers.add("70");
+	    System.out.println("LinkedList: " + numbers);
+	  }
+	}
+
+	public void add(String string) {
+		// TODO Auto-generated method stub
+		
+	}
+}
+	
